4374c43c00c8ac7bfbf92d0f75a04664e24dc3bd
baljit64 <64280741+baljit64@users.noreply.github.com>
2021-07-26 16:00:20 +0530

    redux persist define syntax for index.js file

---
+++ index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './components/store/Reducers/index'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+let store = createStore(persistedReducer)
+let persistor = persistStore(store)
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
+
+
